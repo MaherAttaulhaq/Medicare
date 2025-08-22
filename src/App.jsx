2cf9865d3dd49a0d650949009e3b9d3b78f4ffcc
Attaulhaq <attaulhaq150@gmail.com>
2025-08-22 22:06:20 +0500

    signup

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/signup",
+        path: "/Signup",
         element: <Signup />,
       },
       {
